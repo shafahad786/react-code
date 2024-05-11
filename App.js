3bc85b906d34aa90f0f89737670ad3883db84ebe
shafahad786 <shafahad083@gmail.com>
2024-05-11 14:43:15 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,19 @@
+/*const heading=React.createElement("h1",{},"hello world from react");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);*/
+
+//Nested tags in  react
+
+const parent=React.createElement(
+    "div",
+{id:"parent"},
+ React.createElement(
+    "div"
+ ,{id:"child"},
+ React.createElement(
+    "h1",{id:"heading"},"hi i am h1 tag")
+)
+);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
